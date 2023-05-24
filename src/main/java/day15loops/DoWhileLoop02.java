60fc3a07271dff2541d633c3916555930d04250e
Umit Uygur <umituygur34@gmail.com>
2023-05-24 23:20:15 +0300

    24.05.2023

--- src/main/java/day15loops/DoWhileLoop02.java
+++ src/main/java/day15loops/DoWhileLoop02.java
@@ -1,5 +1,8 @@
 package day15loops;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class DoWhileLoop02 {
     public static void main(String[] args) {
            /*
@@ -7,5 +10,24 @@ public class DoWhileLoop02 {
             Tamsayi 100'den kucuk ise kullaniciya "Kaybettiniz!" mesaji vererek oyunu sonlandiriniz
             Tamsayi 100 ve 100'den buyuk ise "Kazandiniz!" mesaji vererek oyuna devam ettiriniz
          */
+
+        Scanner input = new Scanner(System.in);
+
+
+        do {
+
+            System.out.println("Bir sayi giriniz");
+            int num = input.nextInt();
+
+            if (num >= 100) {
+                System.out.println("KAZANDINIZ:::");
+            } else {
+                System.out.println("KAYBETTİNİZ.");
+                break;
+            }
+
+        } while (true);
+
+
     }
 }
---
+++ src/main/java/day15loops/DoWhileLoop03.java
@@ -0,0 +1,39 @@
+package day15loops;
+
+import java.util.Scanner;
+
+public class DoWhileLoop03 {
+    public static void main(String[] args) {
+          /*
+        Gecerli Username="admin" ve Password="pwd123" dur.
+        Kullanicidan username ve password'u alin.
+        Username ve password dogru ise console'a "Hesabiniza hosgeldiniz!" yazdirin.
+        Username veya password yanlis ise 4 kere "Username ve password'unuzu giriniz" mesaji versin
+        Username veya password 4. kere yanlis girilirse "Hesabiniz bloke olmustur" mesaji vererek islemi tamamlayiniz
+ */
+
+
+        Scanner input = new Scanner(System.in);
+        int counter = 0;
+
+        do {
+
+            if (counter == 4) {
+                System.out.println("Hesabınız bloke olmuştur !! ");
+            }
+            System.out.println("Username giriniz.");
+            String userName = input.next();
+
+            System.out.println("Password giriniz");
+            String password = input.next();
+
+            if (userName.equals("admin") && password.equals("pwd123")) {
+                System.out.println("Hesabınıza hoşgeldiniz");
+                break;
+            }
+            counter++;
+
+
+        } while (true);
+    }
+}
