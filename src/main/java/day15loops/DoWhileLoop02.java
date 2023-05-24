a9b1ec732fcfb54ca1a5ff42ad9bc4a8f24cbe25
Umit Uygur <umituygur34@gmail.com>
2023-05-24 22:51:21 +0300

    24.05.2023

---
+++ src/main/java/day15loops/DoWhileLoop02.java
@@ -0,0 +1,11 @@
+package day15loops;
+
+public class DoWhileLoop02 {
+    public static void main(String[] args) {
+           /*
+            Kullanicidan bir tamsayi aliniz
+            Tamsayi 100'den kucuk ise kullaniciya "Kaybettiniz!" mesaji vererek oyunu sonlandiriniz
+            Tamsayi 100 ve 100'den buyuk ise "Kazandiniz!" mesaji vererek oyuna devam ettiriniz
+         */
+    }
+}
---
+++ src/main/java/derssonrasipractice/Practice24052023.java
@@ -0,0 +1,8 @@
+package derssonrasipractice;
+
+import java.util.Scanner;
+
+public class Practice24052023 {
+
+
+}
