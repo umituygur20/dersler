b3c02974cd46149ac81f1bc38f2795437ade3734
Umit Uygur <umituygur34@gmail.com>
2023-06-05 13:39:33 +0300

    06052023

---
+++ src/main/java/day20statickeywordconstructor/TeacherRunner.java
@@ -0,0 +1,12 @@
+package day20statickeywordconstructor;
+
+public class TeacherRunner {
+    public static void main(String[] args) {
+
+        Teacher teacher = new Teacher("Ali Can", 33, "Ankara");
+        Teacher teacher1 = new Teacher("Veli Khan", 18);
+        System.out.println(teacher);
+        System.out.println(teacher1);
+
+    }
+}
